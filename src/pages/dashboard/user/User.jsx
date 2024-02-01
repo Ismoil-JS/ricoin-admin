57e474be604d585820edf7fa1898f3481f28297b
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-02-01 16:41:27 +0500

    cfoin adder added

--- src/pages/dashboard/user/User.jsx
+++ src/pages/dashboard/user/User.jsx
@@ -5,6 +5,8 @@ import c from './User.module.scss';
 
 const User = () => {
   const [userEmail, setUserEmail] = useState('');
+  const [coinEmail, setCoinEmail] = useState('');
+  const [coins, setCoins] = useState();
 
   function loginUser(e) {
     e.preventDefault();
@@ -27,6 +29,40 @@ const User = () => {
       .catch((err) => {
         if(err.response.status === 401) {
           alert("You are not an admin")
+          window.location.reload()
+        } 
+        else if(err.response.status === 409) {
+          alert("User not exists with this email!")
+        }
+        else {
+          alert("Something went wrong!")
+        }
+      });
+  }
+
+  function giveCoins(e) {
+    e.preventDefault();
+
+    // Define the headers with the Authorization header containing the token
+    const headers = { 'x-auth-token': localStorage.getItem('token') };
+
+    axios
+      .patch('https://api.ricoin.uz/api/users/coins', {
+        email: coinEmail,
+        coins: +coins
+      }, {
+        headers, // Include headers in the request
+      })
+      .then((response) => 
+      {
+        if(response.status === 204) {
+        alert("User has coins now!")
+        window.location.reload()
+      }}
+      )
+      .catch((err) => {
+        if(err.response.status === 401) {
+          alert("You are not an admin")
         } 
         else if(err.response.status === 409) {
           alert("User not exists with this email!")
@@ -42,6 +78,12 @@ const User = () => {
       <form onSubmit={loginUser} className={c.login__form}>
         <input required type="email" placeholder="Email..." onChange={(e) => setUserEmail(e.target.value)} />
         <button type="submit">Make admin</button>
+      </form>
+
+      <form onSubmit={giveCoins} className={c.login__form}>
+        <input required type="email" placeholder="Email..." onChange={(e) => setCoinEmail(e.target.value)} />
+        <input required type="number" placeholder="Coin amount..." onChange={(e) => setCoins(e.target.value)} />
+        <button type="submit">Give coins</button>
       </form>
     </div>
   );
